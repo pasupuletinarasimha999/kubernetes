4cea7fc30a51ab359150f717070c08a3572000c5
pasupuletinarasimha999 <pasupuletinarasimha999@gmail.com>
2022-09-17 20:34:13 +0530

    modified automation files

--- Deployment_Letsencrypt/Automation_Part2.sh
+++ Deployment_Letsencrypt/Automation_Part2.sh
@@ -6,3 +6,15 @@ kubectl apply -f Kubernetes-Dashboard.yml
 kubectl apply -f Serviceaccount.yaml
 kubectl apply -f ingress.yaml
 kubectl apply -f cert.yaml
+echo Please use URL https://dashboard.narasimhakubernetes.xyz/ to access the Kubernetes dashboard
+cd ..
+cd elastic-kibana
+kubectl apply -f storage.yml
+kubectl apply -f kibana-service.yaml
+kubectl apply -f kibana-deployment.yaml
+kubectl apply -f fluentd-es-ds.yaml
+kubectl apply -f fluentd-es-configmap.yaml
+kubectl apply -f es-service.yaml
+kubectl apply -f es-statefulset.yaml
+kubectl apply -f ingress.yaml
+echo Please use URL https://sample.narasimhakubernetes.xyz/kibana to access the Kibana dashboard
